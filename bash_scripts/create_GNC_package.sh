7430dd8aeb3769bb06448792969eda7debe5e8d1
Alberic de Lajarte <42264772+AlbericLaj@users.noreply.github.com>
2022-02-16 18:51:37 +0100

    removed git tracking when creating new project

--- bash_scripts/create_GNC_package.sh
+++ bash_scripts/create_GNC_package.sh
@@ -6,6 +6,7 @@ packageName="${packageName,,}"
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 find "${SCRIPTPATH}/.." -type f -exec sed -i -e 's/template_/'"$packageName"'_/g' {} \;
 
+rm -rf ${SCRIPTPATH}/../.git
 
 mv ${SCRIPTPATH}/../src/template_control.cpp ${SCRIPTPATH}/../src/${packageName}_control.cpp 
 mv ${SCRIPTPATH}/../src/template_navigation.cpp ${SCRIPTPATH}/../src/${packageName}_navigation.cpp 
@@ -17,6 +18,3 @@ mv ${SCRIPTPATH}/../launch/template_SIL.launch ${SCRIPTPATH}/../launch/${package
 mv ${SCRIPTPATH}/../../template_gnc ${SCRIPTPATH}/../../${packageName}_gnc 
 
 echo "Updated name of package to $packageName""_gnc"
-
-
-
