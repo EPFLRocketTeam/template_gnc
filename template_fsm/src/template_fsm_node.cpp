cf4348922ab9f3e7174b905d3d0f0b43db946521
Raphlins <raphael.secondaire@gmail.com>
2022-04-18 15:04:41 +0200

    Adapt template fsm for new message types

--- template_fsm/src/template_fsm_node.cpp
+++ template_fsm/src/template_fsm_node.cpp
@@ -16,38 +16,35 @@
 */
 
 #include "ros/ros.h"
-#include "real_time_simulator/FSM.h"
-#include "real_time_simulator/State.h"
-#include "real_time_simulator/Sensor.h"
-#include "real_time_simulator/GimbalMsg.h"
+#include "rocket_utils/FSM.h"
+#include "rocket_utils/State.h"
+#include "rocket_utils/Sensor.h"
+#include "rocket_utils/GimbalControl.h"
 
 #include <time.h>
 
 #include <sstream>
 #include <string>
 
-#include "real_time_simulator/GetFSM.h"
 #include "std_msgs/String.h"
 
 class FsmNode {
 	private:
 
 		// Current time and state machine
-		real_time_simulator::FSM rocket_fsm;
-		double time_zero;
+		rocket_utils::FSM rocket_fsm;
+		ros::Time time_zero;
 
 		// Last received sensor data
-		real_time_simulator::Sensor rocket_sensor;
+		rocket_utils::Sensor rocket_sensor;
 
 		// Last received feedback control
-		real_time_simulator::GimbalMsg gimbal_state;
+		rocket_utils::GimbalControl gimbal_state;
 
 		// Last received rocket state
-		real_time_simulator::State rocket_state;
+		rocket_utils::State rocket_state;
 
 		// List of subscribers and publishers
-		ros::ServiceServer timer_service;
-
 		ros::Publisher timer_pub;
 
 		ros::Subscriber rocket_state_sub;
@@ -55,7 +52,7 @@ class FsmNode {
 		ros::Subscriber control_sub;
 
 		// Other parameters
-		float rail_length = 0;
+		double rail_length = 0;
 
 	public:
 		FsmNode(ros::NodeHandle &nh)
@@ -64,7 +61,6 @@ class FsmNode {
         	initTopics(nh);
 
 			// Initialize fsm
-			rocket_fsm.time_now = 0;
 			rocket_fsm.state_machine = "Idle";
 
 			// Overwrite rocket mass to stay in launch mode at first iteration
@@ -78,11 +74,8 @@ class FsmNode {
 
 		void initTopics(ros::NodeHandle &nh) 
 		{
-			// Create timer service
-			timer_service = nh.advertiseService("getFSM_gnc", &FsmNode::sendFSM, this);
-
 			// Create timer publisher and associated thread (100Hz)
-			timer_pub = nh.advertise<real_time_simulator::FSM>("gnc_fsm_pub", 10);
+			timer_pub = nh.advertise<rocket_utils::FSM>("gnc_fsm_pub", 10);
 
 			// Subscribe to state message
 			rocket_state_sub = nh.subscribe("kalman_rocket_state", 1, &FsmNode::rocket_stateCallback, this);
@@ -94,7 +87,7 @@ class FsmNode {
 			control_sub = nh.subscribe("gimbal_command_0", 1, &FsmNode::controlCallback, this);
 		}
 
-		void rocket_stateCallback(const real_time_simulator::State::ConstPtr& new_rocket_state)
+		void rocket_stateCallback(const rocket_utils::State::ConstPtr& new_rocket_state)
 		{
 			rocket_state.pose = new_rocket_state->pose;
 			rocket_state.twist = new_rocket_state->twist;
@@ -102,7 +95,7 @@ class FsmNode {
 		}
 
 		// Callback function to store last received sensor data
-		void sensorCallback(const real_time_simulator::Sensor::ConstPtr& new_sensor)
+		void sensorCallback(const rocket_utils::Sensor::ConstPtr& new_sensor)
 		{
 			rocket_sensor.IMU_acc = new_sensor->IMU_acc;
 			rocket_sensor.IMU_gyro = new_sensor->IMU_gyro;
@@ -110,23 +103,11 @@ class FsmNode {
 		}
 
 		// Callback function to store last received commanded control
-		void controlCallback(const real_time_simulator::GimbalMsg::ConstPtr& state)
+		void controlCallback(const rocket_utils::GimbalControl::ConstPtr& state)
 		{
 			gimbal_state.outer_angle = state->outer_angle;
 			gimbal_state.inner_angle = state->inner_angle;
 			gimbal_state.thrust = state->thrust;
-		}
-
-		// Service function: send back fsm (time + state machine)
-		bool sendFSM(real_time_simulator::GetFSM::Request &req, real_time_simulator::GetFSM::Response &res)
-		{
-			// Update current time
-			if (rocket_fsm.state_machine.compare("Idle") != 0) rocket_fsm.time_now = ros::Time::now().toSec() - time_zero;
-
-			res.fsm.time_now = rocket_fsm.time_now;
-			res.fsm.state_machine = rocket_fsm.state_machine;
-			
-			return true;
 		}
 
 		void updateFSM()
@@ -138,14 +119,15 @@ class FsmNode {
 				{
 
 					rocket_fsm.state_machine = "Rail";
-					time_zero = ros::Time::now().toSec();
+					time_zero = ros::Time::now();
 				}
 			}
 
 			else
 			{
 				// Update current time
-				rocket_fsm.time_now = ros::Time::now().toSec() - time_zero;
+				rocket_fsm.header.stamp = ros::Time::now();
+                rocket_fsm.launch_time = time_zero;
 				
 				if (rocket_fsm.state_machine.compare("Rail") == 0)
 				{
